1cd7e9aae5d6ec3f8c20da9d3c48e4b605b99ba1
David Taylor <david.taylor@juul.com>
2024-11-01 16:50:10 -0700

    Propagate delegate errors

--- lib/Sources/BluetoothClient/Errors.swift
+++ lib/Sources/BluetoothClient/Errors.swift
@@ -4,10 +4,20 @@ import JsMessage
 extension BluetoothError: DomErrorConvertable {
     public var domErrorName: DomErrorName {
         switch self {
+        case .cancelled: .abort
         case .causedBy: .unknown
         case .noSuchDevice: .notFound
+        case .noSuchService: .notFound
         case .unavailable: .unknown
         case .unknown: .unknown
+        }
+    }
+}
+
+extension DelegateEventError: DomErrorConvertable {
+    public var domErrorName: DomErrorName {
+        switch self {
+        case .causedBy: .operataion
         }
     }
 }
