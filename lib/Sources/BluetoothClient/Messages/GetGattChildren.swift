53106ccf4f05106de33ec8365c13a4881013dc8b
Travis Wyatt <travis@juul.com>
2024-11-01 16:33:55 -0700

    CA-3639: Support `BluetoothRemoteGATTServer.getPrimaryService`

--- lib/Sources/BluetoothClient/Messages/GetGattChildren.swift
+++ lib/Sources/BluetoothClient/Messages/GetGattChildren.swift
@@ -2,26 +2,30 @@ import Bluetooth
 import Foundation
 import JsMessage
 
-struct GetPrimaryServicesRequest: JsMessageDecodable, PeripheralIdentifiable {
+struct GetGattChildrenRequest: JsMessageDecodable, PeripheralIdentifiable {
     let peripheralId: UUID
+    let single: Bool
     let serviceUuid: UUID?
 
     static func decode(from data: [String: JsType]?) -> Self? {
         guard let uuid = data?["uuid"]?.string.flatMap(UUID.init(uuidString:)) else {
             return nil
         }
+        guard let single = data?["single"]?.number?.boolValue else {
+            return nil
+        }
         let serviceUuid = data?["bluetoothServiceUUID"]?.string.flatMap(UUID.init(uuidString:))
-        return .init(peripheralId: uuid, serviceUuid: serviceUuid)
+        return .init(peripheralId: uuid, single: single, serviceUuid: serviceUuid)
     }
 }
 
-struct GetPrimaryServicesResponse: JsMessageEncodable {
+struct GetGattChildrenResponse: JsMessageEncodable {
     let peripheralId: UUID
-    let primaryServices: [Service]
+    let services: [Service]
 
     func toJsMessage() -> JsMessage.JsMessageResponse {
         return .body([
-            "services": primaryServices.map { $0.uuid.uuidString }
+            "services": services.map { $0.uuid.uuidString }
         ])
     }
 }
