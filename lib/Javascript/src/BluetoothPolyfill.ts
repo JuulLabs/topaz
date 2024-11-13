9514ce3a2012beafe0bcff9802cf1e5d003005ae
Travis Wyatt <travis@juul.com>
2024-11-13 12:46:09 -0800

    Provide `BluetoothUUID`

--- lib/Javascript/src/BluetoothPolyfill.ts
+++ lib/Javascript/src/BluetoothPolyfill.ts
@@ -1,8 +1,14 @@
 // Polyfill for https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth
 
 import { Topaz } from "./Topaz";
+import { BluetoothUUID } from "./BluetoothUUID";
 
 if (typeof((navigator as any).bluetooth) === 'undefined') {
     globalThis.topaz = new Topaz();
     (navigator as any).bluetooth = globalThis.topaz.bluetooth;
+}
+
+if (typeof((window as any).BluetoothUUID) === 'undefined') {
+    globalThis.BluetoothUUID = BluetoothUUID;
+    (window as any).BluetoothUUID = globalThis.BluetoothUUID;
 }
