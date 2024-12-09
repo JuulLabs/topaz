437310daa64ee32a5c370194dc8a985ba68222b0
Justin <gsstuntman@gmail.com>
2024-12-09 11:46:52 -0800

    Update lib/Sources/App/NavBarModel.swift
    Co-authored-by: Travis Wyatt <travis@juul.com>

--- lib/Sources/App/NavBarModel.swift
+++ lib/Sources/App/NavBarModel.swift
@@ -24,7 +24,7 @@ public final class NavBarModel {
     ) {
         self.navigator = navigator
         self.settingsModel = SettingsModel()
-        self.bluetoothStateStream = bleStateStream
+        self.bluetoothStateStream = bluetoothStateStream
         self.settingsModel.dismiss = { [weak self] in
             self?.isSettingsPresented = false
         }
