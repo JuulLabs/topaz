c9969fe8b54fb9701325d345a1a06977213178e4
Travis Wyatt <travis@juul.com>
2025-01-10 15:01:49 -0800

    Fix typo in "operation" spelling (#71)

--- lib/Sources/JsMessage/DomErrorName.swift
+++ lib/Sources/JsMessage/DomErrorName.swift
@@ -4,6 +4,6 @@ public enum DomErrorName: String, Sendable, Encodable {
     case abort = "AbortError"
     case encoding = "EncodingError"
     case notFound = "NotFoundError"
-    case operataion = "OperationError"
+    case operation = "OperationError"
     case unknown = "UnknownError"
 }
