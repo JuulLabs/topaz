f5bc29fd3f18c05af3609ce79a1ac0b85721e421
Justin Tiffner <justin.tiffner@juul.com>
2025-01-14 17:33:05 -0600

    Move CharacteristicRequest to its own file.

--- lib/Sources/BluetoothAction/ReadCharacteristic.swift
+++ lib/Sources/BluetoothAction/ReadCharacteristic.swift
@@ -4,36 +4,6 @@ import BluetoothMessage
 import Foundation
 import JsMessage
 
-struct CharacteristicRequest: JsMessageDecodable {
-    let peripheralId: UUID
-    let serviceUuid: UUID
-    let characteristicUuid: UUID
-    let characteristicInstance: UInt32
-
-    static func decode(from data: [String: JsType]?) -> Self? {
-        guard let device = data?["device"]?.string.flatMap(UUID.init(uuidString:)) else {
-            return nil
-        }
-        guard let service = data?["service"]?.string.flatMap(UUID.init(uuidString:)) else {
-            return nil
-        }
-        guard let characteristic = data?["characteristic"]?.string.flatMap(UUID.init(uuidString:)) else {
-            return nil
-        }
-        guard let instance = data?["instance"]?.number?.uint32Value else {
-            return nil
-        }
-        return .init(peripheralId: device, serviceUuid: service, characteristicUuid: characteristic, characteristicInstance: instance)
-    }
-}
-
-// Response is unused on JavaScript side but needed to have `ReadCharacteristic` conform to `BluetoothAction`.
-struct CharacteristicResponse: JsMessageEncodable {
-    func toJsMessage() -> JsMessage.JsMessageResponse {
-        .body([:])
-    }
-}
-
 struct ReadCharacteristic: BluetoothAction {
     let requiresReadyState: Bool = true
     let request: CharacteristicRequest
