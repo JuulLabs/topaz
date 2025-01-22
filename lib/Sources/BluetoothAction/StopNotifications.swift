e676fa931f2760554cb7ce2948454973a75e7a97
David Taylor <david.taylor@juul.com>
2025-01-22 11:07:18 -0800

    Require connected state for stop notifications

--- lib/Sources/BluetoothAction/StopNotifications.swift
+++ lib/Sources/BluetoothAction/StopNotifications.swift
@@ -13,8 +13,7 @@ struct StopNotifications: BluetoothAction {
     }
 
     func execute(state: BluetoothState, client: any BluetoothClient) async throws -> CharacteristicResponse {
-        let peripheral = try await state.getPeripheral(request.peripheralId)
-        // check: do we need to check for is connected here?
+        let peripheral = try await state.getConnectedPeripheral(request.peripheralId)
         let characteristic = try await state.getCharacteristic(peripheralId: request.peripheralId, serviceId: request.serviceUuid, characteristicId: request.characteristicUuid, instance: request.characteristicInstance)
         _ = try await client.characteristicNotify(peripheral, characteristic: characteristic, enable: false)
         return CharacteristicResponse()
