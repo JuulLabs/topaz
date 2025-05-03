b88e8eee6578c67857a82d86cf1b91a9064d0fb9
David Taylor <david.taylor@juul.com>
2025-05-02 19:43:46 -0700

    Split action unit tests into own module

--- lib/Sources/BluetoothAction/DiscoverCharacteristics.swift
+++ lib/Sources/BluetoothAction/DiscoverCharacteristics.swift
@@ -53,7 +53,6 @@ struct DiscoverCharacteristicsRequest: JsMessageDecodable {
 }
 
 struct DiscoverCharacteristicsResponse: JsMessageEncodable {
-    let peripheralId: UUID
     let characteristics: [Characteristic]
 
     func toJsMessage() -> JsMessage.JsMessageResponse {
@@ -76,9 +75,9 @@ struct DiscoverCharacteristics: BluetoothAction {
             guard let characteristic = result.characteristics.first else {
                 throw BluetoothError.noSuchCharacteristic(service: request.serviceUuid, characteristic: characteristicUuid)
             }
-            return DiscoverCharacteristicsResponse(peripheralId: peripheral.id, characteristics: [characteristic])
+            return DiscoverCharacteristicsResponse(characteristics: [characteristic])
         case .all:
-            return DiscoverCharacteristicsResponse(peripheralId: peripheral.id, characteristics: result.characteristics)
+            return DiscoverCharacteristicsResponse(characteristics: result.characteristics)
         }
     }
 }
