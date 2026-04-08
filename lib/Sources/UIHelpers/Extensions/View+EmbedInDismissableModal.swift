e9c8e969af266ded0f4757cdb42f2e8c82213235
Justin Tiffner <justin.tiffner@juul.com>
2026-04-08 14:52:11 -0500

    Use a ViewModifier for the embedInDismissableModal extension.

--- lib/Sources/UIHelpers/Extensions/View+EmbedInDismissableModal.swift
+++ lib/Sources/UIHelpers/Extensions/View+EmbedInDismissableModal.swift
@@ -2,11 +2,22 @@ import SwiftUI
 
 extension View {
 
-    @ViewBuilder public func embedInDismissableModal(
+    public func embedInDismissableModal(
         trailingPadding: CGFloat,
         yOffset: CGFloat,
         onTapOutside: @escaping () -> Void
     ) -> some View {
+        modifier(EmbeddedDissmissableModal(trailingPadding: trailingPadding, yOffset: yOffset, onTapOutside: onTapOutside))
+    }
+}
+
+struct EmbeddedDissmissableModal: ViewModifier {
+    
+    let trailingPadding: CGFloat
+    let yOffset: CGFloat
+    let onTapOutside: () -> Void
+    
+    func body(content: Content) -> some View {
         ZStack(alignment: .bottomTrailing) {
             Color.clear
                 .contentShape(Rectangle()) // Ensures the entire area is tappable
@@ -14,7 +25,7 @@ extension View {
                 .onTapGesture {
                     onTapOutside()
                 }
-            self
+            content
         }
         .padding(.trailing, trailingPadding)
         .offset(y: yOffset)
