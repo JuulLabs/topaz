06f75b485a2cd70bf9d922615155ac8d8143f11a
David Taylor <david.taylor@juul.com>
2026-06-18 15:26:18 -0700

    lintage

--- lib/Sources/BluetoothNative/BluetoothPower.swift
+++ lib/Sources/BluetoothNative/BluetoothPower.swift
@@ -4,5 +4,5 @@ public func liveBluetoothPowerRequest() {
     let options: [String: Any] = [
         CBCentralManagerOptionShowPowerAlertKey: true as NSNumber
     ]
-    let _ = CBCentralManager(delegate: nil, queue: nil, options: options)
+    _ = CBCentralManager(delegate: nil, queue: nil, options: options)
 }
